7dbd7207af8d557258eeef7731111494522b7df2
Satya Prakash <165584094+Satyaprakash666@users.noreply.github.com>
2026-03-16 00:17:51 +0530

    Update firebase.js

--- static/js/firebase.js
+++ static/js/firebase.js
@@ -1,23 +1,21 @@
-// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBEPh61ad4j9qogxH457h79NE9AQJD_tUo",
-    authDomain: "helpforfarmer-c717e.firebaseapp.com",
-    projectId: "helpforfarmer-c717e",
-    storageBucket: "helpforfarmer-c717e.firebasestorage.app",
-    messagingSenderId: "645087836594",
-    appId: "1:645087836594:web:ce04cb38c529d5fe7d8cab",
-    measurementId: "G-MSVG3D5XTD"
+  apiKey: "AIzaSyCZnohyh3EPHHBCv88l96ddKFcFvHDv7Eo",
+  authDomain: "chat-bot-cea01.firebaseapp.com",
+  projectId: "chat-bot-cea01",
+  storageBucket: "chat-bot-cea01.firebasestorage.app",
+  messagingSenderId: "137917979431",
+  appId: "1:137917979431:web:bfac72a9650341b2866702",
+  measurementId: "G-EQN0SDN3FF"
 };
 
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-// Initialize Firebase Authentication and make it available globally
 const auth = firebase.auth();
 window.auth = auth;
 
-// Set persistence to LOCAL to maintain session
 auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
     .then(() => {
         console.log("Auth persistence set to LOCAL");
@@ -26,21 +24,19 @@ auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
         console.error("Error setting auth persistence:", error);
     });
 
-// Debug initialization
 console.log("Firebase initialized successfully");
 console.log("Auth object:", auth); 
 firebase.auth().onAuthStateChanged((user) => {
     const currentPath = window.location.pathname;
 
     if (user) {
-        // User is logged in
-        if (!currentPath.includes('home')) {  // redirect only if not already on home
-            window.location.href = "https://chatbot-agent-t22h.onrender.com/home";
+        if (!currentPath.includes('home')) {
+            window.location.href = "/home";
         }
     } else {
         console.log("User is signed out");
-        if (!currentPath.includes('')) { // redirect only if not already on login page
-            window.location.href = "https://chatbot-agent-t22h.onrender.com/login";
+        if (!currentPath.includes('')) {
+            window.location.href = "/login";
         }
     }
 });
